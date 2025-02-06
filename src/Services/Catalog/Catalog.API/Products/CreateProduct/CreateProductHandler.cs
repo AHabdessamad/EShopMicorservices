f152ed077c9d1c5f523695bacf565345540a53ef
AHabdessamad <a.aithamou1046@uca.ac.ma>
2025-02-06 22:01:17 +0100

    setup CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlucks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        // create new command
+        // save to DB
+        // return  Result
+
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+        // TODO
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//Add Services 
+
+
+//Add Configuration to Http request pipeline
+
+app.Run();
